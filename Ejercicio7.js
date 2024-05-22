25dcdf927942a14e04763e77207daf380ee026df
Kiger22 <143103592+Kiger22@users.noreply.github.com>
2024-05-22 12:21:54 +0200

    Create Ejercicio7.js

---
+++ Ejercicio7.js
@@ -0,0 +1,34 @@
+/* 7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+los alumnos usando la función .reduce(). */
+
+const exams = [
+  { name: 'Yuyu Cabeza Crack', score: 5 },
+  { name: 'Maria Aranda Jimenez', score: 1 },
+  { name: 'Cristóbal Martínez Lorenzo', score: 6 },
+  { name: 'Mercedez Regrera Brito', score: 7 },
+  { name: 'Pamela Anderson', score: 3 },
+  { name: 'Enrique Perez Lijó', score: 6 },
+  { name: 'Pedro Benitez Pacheco', score: 8 },
+  { name: 'Ayumi Hamasaki', score: 4 },
+  { name: 'Robert Kiyosaki', score: 2 },
+  { name: 'Keanu Reeves', score: 10 }
+];
+
+const totalScores = exams.reduce((sumScores, exam) => sumScores + exam.score, 0);
+console.log(totalScores);
+
+/* 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+alumnos que esten aprobados usando la función .reduce(). */
+
+const totalScoresOk = exams.reduce((sumScores, exam) => {
+  if (exam.score >= 5) {
+    return sumScores + exam.score;
+  }
+  return sumScores;
+}, 0);
+console.log(totalScoresOk);
+
+/* 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce(). */
+
+const mediaScores = exams.reduce((sumScores, exam) => sumScores + exam.score, 0) / exams.length;
+console.log(mediaScores);
